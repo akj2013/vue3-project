de7f7ce238302ec28ab2e851c68643c1ae8c3cec
akj2013 <48812384+akj2013@users.noreply.github.com>
2022-03-23 18:09:59 +0900

    45퍼센트

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@
     <!-- todo가 없을 때 경고 메세지 -->
     <div v-if="!todos.length" class="mt-4 red">추가된 TODO가 없습니다.</div>
     <!-- 검색 결과가 없을 때 경고 메세지 -->
-    <div v-if="!filteredTodos.length && todos.length" class="mt-4 red">표시할 TODO가 없습니다.</div>
+    <div v-if="!todos.length && todos.length" class="mt-4 red">표시할 TODO가 없습니다.</div>
     <!-- todo 반복문 -->
-    <TodoList :todos="filteredTodos" :numberOfPages="numberOfPages" :currentPage="currentPage" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" @get-todos="getTodos" />
+    <TodoList :todos="todos" :numberOfPages="numberOfPages" :currentPage="currentPage" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" @get-todos="getTodos" />
   </div>
   <div class="container align-items-center">
     <hr>
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue'; // 데이터 연동
+import { ref, computed, watch } from 'vue'; // 데이터 연동
 
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
@@ -52,7 +52,37 @@ export default {
     const currentPage = ref(1);
     const numberOfPages = computed(() => {
       return Math.ceil(totalTodos.value / pageLimit); // 올림 2.2 = 3페이지
-    })
+    });
+    const searchText = ref('');
+// === watch 예제 ===
+    // const a = reactive({
+    //   b: 1,
+    //   c: 3
+    // });
+    // watch([currentPage, totalTodos], (currentPage, prev) => {
+    //   console.log(currentPage, prev); 
+    // });
+    // watch(() => [a.b, a.c], (current, prev) => {
+    //   // 여러 개를 감지할 경우, 리턴도 배열로 순서대로 들어간다.
+    //   console.log('current : ' + current, 'prev : ' + prev);
+    //   console.log('current : ' + current + ', prev : ' + prev);
+    // });
+    // a.b = 2;
+// === watchEffect 예제 ===
+    // const a = reactive({
+    //   b: 1
+    // });
+    // watchEffect(() => {
+      // watchEffect : currentPage.value가 변경될 때 감지하여, 해당 메소드를 다시 실행한다.
+      // console.log(currentPage.value);
+      // console.log(totalTodos.value);
+      // react, props, computed값이 변경될 때도 실행된다.
+      // console.log(numberOfPages.value);
+      // console.log(a.b);
+      // 그 이외에는 변경되어도 처음만 실행된다.
+      // console.log(limit);
+    // });
+    // a.b = 4;
 
     // db.json으로부터 todos를 가져온다.
     // 리턴하는 함수가 아니다.
@@ -60,7 +90,8 @@ export default {
       currentPage.value = page;
       try {
         // 백틱: ``으로 할 시, 문자열 내부에 자바스크립트 객체를 사용할 수 있다.
-        const res = await axios.get(`http://localhost:3000/todos?_page=${page}&_limit=${pageLimit}`);
+        const res = await axios
+          .get(`http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${pageLimit}`);
         totalTodos.value = res.headers['x-total-count']; // todo 갯수
         todos.value = (res.data);
       } catch(err) {
@@ -86,6 +117,8 @@ export default {
           completed: todo.completed,
         });
         todos.value.push(res.data); // id까지 포함한 데이터가 객체에 저장된다.
+        // 재검색
+        getTodos();
       } catch(err2) {
         console.log(err2);
         error.value = 'POST 송신에 에러가 발생하였습니다.';
@@ -105,6 +138,13 @@ export default {
         await axios.delete('http://localhost:3000/todos/' + id);
         todos.value.splice(index, 1);
         alert('id : ' + id + '가 삭제되었습니다.');
+        // TODO 재검색
+        // 전체 갯수 % 삭제한 후 갯수 == 0 && 현재가 마지막 페이지, 페이지 = 페이지 - 1을 해준다.
+        const res = await axios.get('http://localhost:3000/todos/');
+        if (res.data.length % pageLimit == 0 && currentPage.value === numberOfPages.value) {
+          currentPage.value = currentPage.value - 1;
+        }
+        getTodos(currentPage.value);
       } catch(err) {
         error.value = 'DELETE 송신에 에러가 발생하였습니다.';
       }
@@ -124,16 +164,16 @@ export default {
       }
     };
 
-    const searchText = ref('');
-    const filteredTodos = computed(() => {
-      if (searchText.value) {
-        return todos.value.filter(todo => {
-          return todo.subject.includes(searchText.value);
-        });
-      }
-      // 빈 공간일 경우,
-      return todos.value;
-    });
+    // const searchText = ref('');
+    // const filteredTodos = computed(() => {
+    //   if (searchText.value) {
+    //     return todos.value.filter(todo => {
+    //       return todo.subject.includes(searchText.value);
+    //     });
+    //   }
+    //   // 빈 공간일 경우,
+    //   return todos.value;
+    // });
 
     const showIf = ref(false);
     const toggleShowIf = () => {
@@ -144,6 +184,12 @@ export default {
       showComputed.value = !showComputed.value;
     }
 
+    // 검색조건이 바뀌는지 관찰하고, 바뀌면 재검색한다.
+    // 항상 첫번째 페이지를 표시하고, 검색조건은 getTodos의 get()에 파라미터로 들어가 있다.
+    watch(searchText, () => {
+      getTodos(1);
+    });
+
     return {
       toggle,
       todos,
@@ -152,7 +198,7 @@ export default {
       deleteTodo,
       toggleTodo,
       searchText,
-      filteredTodos,
+      // filteredTodos,
       error,
       showIf,
       toggleShowIf,
